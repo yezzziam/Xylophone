62d2abba312e7e9a2493e1e083e8d02628091392
yezzziam <yezzziam@gmail.com>
2020-02-01 00:59:13 +0200

    Added sounds and UI programmatically

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var key = UIButton()
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        key = UIButton(type: .custom)
+        key.frame = CGRect(x: 100, y: 100, width: 200, height: 80)
+        key.backgroundColor = .blue
+        key.setTitle("C", for: .normal)
+        key.setTitle("c", for: .highlighted)
+        view.addSubview(key)
+        key.addTarget(self, action: #selector(keyPlay(_:)), for: .touchDown)
+        
+    }
+    
+    @objc func keyPlay(_ sender: UIButton) {
+        playSound()
+    }
+  
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
     }
 
 
